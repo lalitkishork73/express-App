ace816c15cba5d988fc0b0d1e0c51efb5926d259
Lalitkishor Kanojiya <lalitkishork73@gmail.com>
2024-04-17 23:29:09 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express API!');
+});
+
+// Define another route
+app.get('/api/data', (req, res) => {
+  const data = {
+    message: 'This is some data from the API!',
+    timestamp: new Date()
+  };
+  res.json(data);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
