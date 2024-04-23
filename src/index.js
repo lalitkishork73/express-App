9c92b5b029f4850e12a31b72df5efc83ed6a5505
Lalitkishor Kanojiya <lalitkishork73@gmail.com>
2024-04-23 12:40:02 +0530

    console version 1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Welcome to my Express API!');
-});
-app.get('/lalit', (req, res) => {
-  res.send({ message: "Welcome to my Express API Jungle you jungly insaan." });
-});
+// // Define a route
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my Express API!');
+// });
+// app.get('/lalit', (req, res) => {
+//   res.send({ message: "Welcome to my Express API Jungle you jungly insaan." });
+// });
 
-// Define another route
-app.get('/api/data', (req, res) => {
-  const data = {
-    message: 'This is some data from the API!',
-    timestamp: new Date()
-  };
-  res.json(data);
-});
+// // Define another route
+// app.get('/api/data', (req, res) => {
+//   const data = {
+//     message: 'This is some data from the API!',
+//     timestamp: new Date()
+//   };
+//   res.json(data);
+// });
 
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port with version 3 ${PORT}`);
-});
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port with version 3 ${PORT}`);
+// });
+
+console.log("version 1")
